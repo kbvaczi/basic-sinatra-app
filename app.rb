02091d189d8a688424af5fd8cb9bf263355c2014
Ken V <kvaczi2@gmail.com>
2016-04-09 08:33:02 -0500

    added super secret easter egg

--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
 require 'sinatra'
 
 class HelloWorldResponder < Sinatra::Base
-  get '/' do
-    "Hello worldy world!"
+
+  get '/?:secret?' do
+    if params[:secret] != nil
+      params[:secret] == "code" ? "You successfully passed the test!" : "No secret for you!"
+    else
+      "Boring hello world, you may have missed something?"
+    end    
   end
+
 end
